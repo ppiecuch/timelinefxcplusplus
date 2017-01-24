690fa7c22416283eb3304c1a5dcf1c2fa5504ee9
Pawel Piecuch <piecuch.pawel@gmail.com>
2017-01-24 16:05:01 +0100

    fixing debug_font drawing

--- ext/debug_font.cpp
+++ ext/debug_font.cpp
@@ -126,27 +126,6 @@ static void _UpdateMessage(int line, const char* fmt, va_list args)
   }
 }
 
-static const int kTextVertBuf = 2048;
-#ifndef DEBUG_FG_BG
-static struct {
-    pt_t v1, t1;
-    pt_t v2, t2;
-    pt_t v3, t3;
-    pt_t v4, t4;
-    pt_t v5, t5;
-    pt_t v6, t6;
-} text[kTextVertBuf];
-#else
-static struct {
-    pt_t v1; union { pt_t t1b; pt_t t1; }; union { pt_t t1f; pt_t r1; }; union { uint32_t c1b; uint32_t c1; }; union { uint32_t c1f; uint32_t a1; };
-    pt_t v2; union { pt_t t2b; pt_t t2; }; union { pt_t t2f; pt_t r2; }; union { uint32_t c2b; uint32_t c2; }; union { uint32_t c2f; uint32_t a2; };
-    pt_t v3; union { pt_t t3b; pt_t t3; }; union { pt_t t3f; pt_t r3; }; union { uint32_t c3b; uint32_t c3; }; union { uint32_t c3f; uint32_t a3; };
-    pt_t v4; union { pt_t t4b; pt_t t4; }; union { pt_t t4f; pt_t r4; }; union { uint32_t c4b; uint32_t c4; }; union { uint32_t c4f; uint32_t a4; };
-    pt_t v5; union { pt_t t5b; pt_t t5; }; union { pt_t t5f; pt_t r5; }; union { uint32_t c5b; uint32_t c5; }; union { uint32_t c5f; uint32_t a5; };
-    pt_t v6; union { pt_t t6b; pt_t t6; }; union { pt_t t6f; pt_t r6; }; union { uint32_t c6b; uint32_t c6; }; union { uint32_t c6f; uint32_t a6; };
-} text[kTextVertBuf];
-#endif
-
 static uint32_t text_colors[16] = {
     /* BLACK        */ 0x00000000,
     /* BLUE         */ 0xFFFF0000,
@@ -167,22 +146,25 @@ static uint32_t text_colors[16] = {
 };
 
 #define _flushLine() \
-	int base = inverted?0x100:0; \
-	for(unsigned char *p = (unsigned char*)string; *p; ++p) { \
-	    if (*p == 0xff) \
-	        base = base^0x100; /* inv on/off */ \
-	    else { \
-	        const int chr = *p+base; \
-	        text[cnt].v1 = xx + v0; text[cnt].t1 = chars[chr].t[0]; \
-	        text[cnt].v2 = xx + v1; text[cnt].t2 = chars[chr].t[1]; \
-	        text[cnt].v3 = xx + v2; text[cnt].t3 = chars[chr].t[2]; \
-	        text[cnt].v4 = xx + v2; text[cnt].t4 = chars[chr].t[2]; \
-	        text[cnt].v5 = xx + v1; text[cnt].t5 = chars[chr].t[1]; \
-	        text[cnt].v6 = xx + v3; text[cnt].t6 = chars[chr].t[3]; \
-	        ++cnt; xx.x += space; \
-        	if (xx.x >= scwidth) break; /* out of the screen */ \
-	    } \
-	}
+    if(text[rev].cnt < kTextVertBuf) { \
+        int base = inverted?0x100:0; \
+        for(unsigned char *p = (unsigned char*)string; *p; ++p) { \
+            if (*p == 0xff) \
+                base = base^0x100; /* inv on/off */ \
+            else { \
+                const int chr = *p+base; \
+                text[rev]().v1 = xx + v0; text[rev]().t1 = chars[chr].t[0]; \
+                text[rev]().v2 = xx + v1; text[rev]().t2 = chars[chr].t[1]; \
+                text[rev]().v3 = xx + v2; text[rev]().t3 = chars[chr].t[2]; \
+                text[rev]().v4 = xx + v2; text[rev]().t4 = chars[chr].t[2]; \
+                text[rev]().v5 = xx + v1; text[rev]().t5 = chars[chr].t[1]; \
+                text[rev]().v6 = xx + v3; text[rev]().t6 = chars[chr].t[3]; \
+                ++text[rev]; xx.x += space; \
+                if (xx.x >= scwidth) break; /* out of the screen */ \
+                if(text[rev].cnt == kTextVertBuf) break; /* out of buffer */ \
+            } \
+        } \
+    }
 
 void dbgFlush()
 {
@@ -205,16 +187,52 @@ void dbgFlush()
     static const pt_t v2 = pt(kDebugFontSizeW, 0);               /* 1, 0 */
     static const pt_t v3 = pt(kDebugFontSizeW, kDebugFontSizeH); /* 1, 1 */
 
-    int cnt = 0; pt_t xx = {0, 0}; for(uint s=0; s<messages.size(); ++s) {
-        const char *string = messages[s].c_str();
-        _flushLine();
-        xx.x = 0; xx.y += kDebugFontSizeH;
-        if (xx.y >= scheight) break; // out of the screen
-    }
-    if (!status.empty())
-    {
-        const char *string = status.c_str();
-        pt_t xx = {0, (max_lines-1)*kDebugFontSizeH}; _flushLine();
+    static const int kTextVertBuf = 2048;
+    #ifndef DEBUG_FG_BG
+    static struct {
+        int cnt;
+        struct text_vert_t {
+            pt_t v1, t1;
+            pt_t v2, t2;
+            pt_t v3, t3;
+            pt_t v4, t4;
+            pt_t v5, t5;
+            pt_t v6, t6;
+        } buf[kTextVertBuf];
+        text_vert_t & operator =(int v) { cnt = v; return buf[cnt]; }
+        text_vert_t & operator ()() { return buf[cnt]; } // reference to current buffer element
+        text_vert_t & operator ++() { return buf[++cnt]; }
+    } text[2] = {0,{0}};
+    #else
+    static struct {
+        int cnt;
+        static struct {
+            pt_t v1; union { pt_t t1b; pt_t t1; }; union { pt_t t1f; pt_t r1; }; union { uint32_t c1b; uint32_t c1; }; union { uint32_t c1f; uint32_t a1; };
+            pt_t v2; union { pt_t t2b; pt_t t2; }; union { pt_t t2f; pt_t r2; }; union { uint32_t c2b; uint32_t c2; }; union { uint32_t c2f; uint32_t a2; };
+            pt_t v3; union { pt_t t3b; pt_t t3; }; union { pt_t t3f; pt_t r3; }; union { uint32_t c3b; uint32_t c3; }; union { uint32_t c3f; uint32_t a3; };
+            pt_t v4; union { pt_t t4b; pt_t t4; }; union { pt_t t4f; pt_t r4; }; union { uint32_t c4b; uint32_t c4; }; union { uint32_t c4f; uint32_t a4; };
+            pt_t v5; union { pt_t t5b; pt_t t5; }; union { pt_t t5f; pt_t r5; }; union { uint32_t c5b; uint32_t c5; }; union { uint32_t c5f; uint32_t a5; };
+            pt_t v6; union { pt_t t6b; pt_t t6; }; union { pt_t t6f; pt_t r6; }; union { uint32_t c6b; uint32_t c6; }; union { uint32_t c6f; uint32_t a6; };
+        } buf[kTextVertBuf];
+        text_vert_t & operator =(int v) { cnt = v; return buf[cnt]; }
+        text_vert_t & operator ()() { return buf[cnt]; } // reference to current buffer element
+        text_vert_t & operator ++() { return buf[++cnt]; }
+    } text[2] = {0,{0}};
+    #endif
+
+    if (changed) {
+        text[0] = 0; text[1] = 0; // reset buffers
+        int rev = 0; pt_t xx = {0, 0}; for(uint s=0; s<messages.size(); ++s) {
+            const char *string = messages[s].c_str();
+            _flushLine();
+            xx.x = 0; xx.y += kDebugFontSizeH;
+            if (xx.y >= scheight) break; // out of the screen
+        }
+        if (!status.empty())
+        {
+            const char *string = status.c_str();
+            pt_t xx = {0, (max_lines-1)*kDebugFontSizeH}; _flushLine();
+        }
     }
 
 	glEnable(GL_TEXTURE_2D);
@@ -238,11 +256,18 @@ void dbgFlush()
     glEnableClientState(GL_TEXTURE_COORD_ARRAY);
 	glEnableClientState(GL_VERTEX_ARRAY);
     
-	glTexCoordPointer(2, GL_FLOAT, 2*sizeof(pt_t), &text->t1);
-	glVertexPointer(2, GL_FLOAT, 2*sizeof(pt_t), &text->v1);
-	glDrawArrays(GL_TRIANGLES, 0, cnt*6);           // 2 triangles per char
+    if (text[0].cnt) {
+        glTexCoordPointer(2, GL_FLOAT, 2*sizeof(pt_t), &text[0].buf->t1);
+        glVertexPointer(2, GL_FLOAT, 2*sizeof(pt_t), &text[0].buf->v1);
+        glDrawArrays(GL_TRIANGLES, 0, text[0].cnt*6);// 2 triangles per char
+    }
+    if (text[1].cnt) {
+        glTexCoordPointer(2, GL_FLOAT, 2*sizeof(pt_t), &text[1].buf->t1);
+        glVertexPointer(2, GL_FLOAT, 2*sizeof(pt_t), &text[1].buf->v1);
+        glDrawArrays(GL_TRIANGLES, 0, text[1].cnt*6);// 2 triangles per char
+    }
 
-	glDisableClientState(GL_VERTEX_ARRAY);
+    glDisableClientState(GL_VERTEX_ARRAY);
 	glDisableClientState(GL_TEXTURE_COORD_ARRAY);
     
     glMatrixMode(GL_PROJECTION);					// select the projection matrix
@@ -276,7 +301,10 @@ void dbgSetStatusLine(const char* fmt, ...)
     va_start(args, fmt);
   	char *message = NULL;
   	vasprintf(&message, fmt, args);
-  	if (message) status = message;
+  	if (message && strcmp(message, status.c_str())!=0) {
+        status = message;
+        changed = true;
+    }
     va_end(args);
 }
 
